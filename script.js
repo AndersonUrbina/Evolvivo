57a3518f5eac15e938f28099accea17b40e08726
AndersonUrbina <urbina.du60944@gmail.com>
2025-05-29 22:37:37 -0500

    script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             date: 'March 14, 2024',
             readTime: '5 min read',
             imagePlaceholder: '600 x 400'
+            imageUrl: 'https://images.pexels.com/photos/4964985/pexels-photo-4964985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
         },
         {
             category: 'Goal Setting',
